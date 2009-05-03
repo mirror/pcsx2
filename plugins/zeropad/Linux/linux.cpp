mirror/pcsx2

commit 611a79747b9a8fdb63db517beefff536b8afe43e

ZeroPad: Apply the multiple key patch in issue 197 to ZeroPad. Currently Linux only.
git-svn-id: http://pcsx2.googlecode.com/svn/trunk@1125 96395faa-99c1-11dd-bbfe-3dabce05a288

plugins/zeropad/Linux/linux.cpp
@@ -103,7 +103,7 @@ void SaveConfig()
         return;
     }
     
-	for (j=0; j<2; j++) {
+	for (j=0; j<2 * PADSUBKEYS; j++) {
 		for (i=0; i<PADKEYS; i++) {
 			fprintf(f, "[%d][%d] = 0x%lx\n", j, i, conf.keys[j][i]);
 		}
@@ -127,12 +127,12 @@ string GetLabelFromButton(const char* buttonname)
 }
 
 void LoadConfig() {
-    FILE *f;
+	FILE *f;
 	char str[256];
-    char cfg[255];
+	char cfg[255];
 	int i, j;
 
-    memset(&conf, 0, sizeof(conf));
+	memset(&conf, 0, sizeof(conf));
 	conf.keys[0][0] = XK_a;			// L2
 	conf.keys[0][1] = XK_semicolon;			// R2
 	conf.keys[0][2] = XK_w;			// L1
@@ -149,23 +149,24 @@ void LoadConfig() {
 	conf.keys[0][15] = XK_s;	// LEFT
 	conf.log = 0;
 
-    strcpy(cfg, s_strIniPath.c_str());
-    f = fopen(cfg, "r");
-    if (f == NULL) {
-        printf("ZeroPAD: failed to load ini %s\n", s_strIniPath.c_str());
-        SaveConfig();//save and return
-        return;
-    }
+	strcpy(cfg, s_strIniPath.c_str());
+	f = fopen(cfg, "r");
+	if (f == NULL) {
+	        printf("ZeroPAD: failed to load ini %s\n", s_strIniPath.c_str());
+        	SaveConfig();//save and return
+	        return;
+	}
 
-	for (j=0; j<2; j++) {
+	for (j=0; j < 2 * PADSUBKEYS; j++) {
 		for (i=0; i<PADKEYS; i++) {
 			sprintf(str, "[%d][%d] = 0x%%x\n", j, i);
-			fscanf(f, str, &conf.keys[j][i]);
+			if (fscanf(f, str, &conf.keys[j][i]) == 0)
+				conf.keys[j][i] = 0;
 		}
 	}
-    fscanf(f, "log = %d\n", &conf.log);
-    fscanf(f, "options = %d\n", &conf.options);
-    fclose(f);
+	fscanf(f, "log = %d\n", &conf.log);
+	fscanf(f, "options = %d\n", &conf.options);
+	fclose(f);
 }
 
 GtkWidget *MsgDlg;
@@ -251,16 +252,16 @@ void _PADupdate(int pad)
 
 int _GetJoystickIdFromPAD(int pad)
 {
-    // select the right joystick id
-    int joyid = -1;
-    for(int i = 0; i < PADKEYS; ++i) {
-        if( IS_JOYSTICK(conf.keys[pad][i]) || IS_JOYBUTTONS(conf.keys[pad][i]) ) {
-            joyid = PAD_GETJOYID(conf.keys[pad][i]);
-            break;
-        }
-    }
-
-    return joyid;
+	// select the right joystick id
+	int joyid = -1;
+	for (int p = 0; p < PADSUBKEYS; p++) {
+		for(int i = 0; i < PADKEYS; ++i) {
+	       		if (IS_JOYSTICK(conf.keys[(PadEnum[pad][p])][i]) || IS_JOYBUTTONS(conf.keys[(PadEnum[pad][p])][i])) {
+			        joyid = PAD_GETJOYID(conf.keys[(PadEnum[pad][p])][i]);
+				return joyid;
+		        }
+		}
+	}
 }
 
 void CALLBACK PADupdate(int pad)
@@ -275,30 +276,20 @@ void CALLBACK PADupdate(int pad)
 		XNextEvent(GSdsp, &E);
 		switch (E.type) {
         case KeyPress:
-            //_KeyPress(pad, XLookupKeysym((XKeyEvent *)&E, 0)); break;
             key = XLookupKeysym((XKeyEvent *)&E, 0);
-            for (i=0; i<PADKEYS; i++) {
-                if (key == conf.keys[pad][i]) {
-                    keyPress|=(1<<i);
-                    keyRelease&=~(1<<i);
-                    break;
-                }
-            }
-            
+
+	    i = FindKey(key, pad);
+            keyPress |= (1 << i);
+            keyRelease &= ~(1 << i);
+           
             event.evt = KEYPRESS;
             event.key = key;
             break;
         case KeyRelease:
             key = XLookupKeysym((XKeyEvent *)&E, 0);
-            //_KeyRelease(pad, XLookupKeysym((XKeyEvent *)&E, 0));
-            for (i=0; i<PADKEYS; i++) {
-                if (key == conf.keys[pad][i]) {
-                    keyPress&=~(1<<i);
-                    keyRelease|= (1<<i);
-                    break;
-                }
-            }
-            
+	    i = FindKey(key, pad); 
+            keyPress &= ~(1<<i);
+            keyRelease |= (1<<i);
             event.evt = KEYRELEASE;
             event.key = key;
             break;
@@ -316,24 +307,24 @@ void CALLBACK PADupdate(int pad)
     // joystick info
 #ifdef JOYSTICK_SUPPORT
 
-    SDL_JoystickUpdate();
-    for (int i=0; i<PADKEYS; i++) {
-        int key = conf.keys[pad][i];
-        JoystickInfo* pjoy = NULL;
+	SDL_JoystickUpdate();
+	for (int i=0; i<PADKEYS; i++) {
+	        int key = conf.keys[PadEnum[pad][0]][i];
+		JoystickInfo* pjoy = NULL;
         
-        if( IS_JOYBUTTONS(key) ) {
-            int joyid = PAD_GETJOYID(key);
-            if( joyid >= 0 && joyid < (int)s_vjoysticks.size()) {
-                pjoy = s_vjoysticks[joyid];
-                if( SDL_JoystickGetButton((pjoy)->GetJoy(), PAD_GETJOYBUTTON(key)) ) {
-                    status[(pjoy)->GetPAD()] &= ~(1<<i); // pressed
-                }
-                else
-                    status[(pjoy)->GetPAD()] |= (1<<i); // pressed
-            }
-        }
-        else if( IS_JOYSTICK(key) ) {
-            int joyid = PAD_GETJOYID(key);
+		if( IS_JOYBUTTONS(key) ) {
+	            	int joyid = PAD_GETJOYID(key);
+        	    	if( joyid >= 0 && joyid < (int)s_vjoysticks.size()) {
+                		pjoy = s_vjoysticks[joyid];
+	        	        if( SDL_JoystickGetButton((pjoy)->GetJoy(), PAD_GETJOYBUTTON(key)) ) {
+        			        status[(pjoy)->GetPAD()] &= ~(1<<i); // pressed
+	                	}
+		                else
+        			        status[(pjoy)->GetPAD()] |= (1<<i); // pressed
+			}
+        	}
+	        else if( IS_JOYSTICK(key) ) {
+        	int joyid = PAD_GETJOYID(key);
             if( joyid >= 0 && joyid < (int)s_vjoysticks.size()) {
 
                 pjoy = s_vjoysticks[joyid];
@@ -411,10 +402,10 @@ static int s_selectedpad = 0;
 
 void UpdateConf(int pad)
 {
-    s_selectedpad = pad;
+	s_selectedpad = pad;
 
 	int i;
-    GtkWidget *Btn;
+	GtkWidget *Btn;
 	for (i=0; i<ArraySize(s_pGuiKeyMap); i++) {
         
         if( s_pGuiKeyMap[i] == NULL )
@@ -583,6 +574,18 @@ void OnConf_Pad2(GtkButton *button, gpointer user_data)
 {
     if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button)) )
         UpdateConf(1);
+}
+
+void OnConf_Pad3(GtkButton *button, gpointer user_data)
+{
+	if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button)) )
+	        UpdateConf(2);
+}
+
+void OnConf_Pad4(GtkButton *button, gpointer user_data)
+{
+	if( gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button)) )
+	        UpdateConf(3);
 }
 
 void OnConf_Ok(GtkButton *button, gpointer user_data)

plugins/zeropad/Windows/win.cpp
@@ -54,7 +54,7 @@ void SaveConfig()
 	if(!szTemp) return;
 	strcpy(szTemp, "\\inis\\zeropad.ini");
 
-	for (j=0; j<2; j++) {
+	for (j=0; j<2 * PADSUBKEYS; j++) {
 		for (i=0; i<PADKEYS; i++) {
             sprintf(szProf, "%d_%d", j, i);
 			sprintf(szValue, "%d", conf.keys[j][i]);
@@ -105,7 +105,7 @@ void LoadConfig()
 	}
 	fclose(fp);
 
-    for (j=0; j<2; j++) {
+    for (j=0; j<2 * PADSUBKEYS; j++) {
 		for (i=0; i<PADKEYS; i++) {
             sprintf(szProf, "%d_%d", j, i);
             GetPrivateProfileString("Interface", szProf, NULL, szValue, 20, szIniFile);
@@ -181,33 +181,19 @@ LRESULT WINAPI PADwndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			if (lParam & 0x40000000)
 				return TRUE;
 
-            for(pad = 0; pad < 2; ++pad ) {
-                for (i=0; i<PADKEYS; i++) {
-                    if (wParam == conf.keys[pad][i]) {
+			i = FindKey(wparam, pad);
                         keyPress[pad] |=(1<<i);
                         keyRelease[pad] &=~(1<<i);
-                        break;
-                    }
-                }
-            }
-
-            event.evt = KEYPRESS;
-            event.key = wParam;
+   		       	event.evt = KEYPRESS;
+			event.key = wParam;
 			break;
 
 		case WM_KEYUP:
-            for(pad = 0; pad < 2; ++pad ) {
-			    for (i=0; i<PADKEYS; i++) {
-                    if (wParam == conf.keys[pad][i]) {
+			i = FindKey(wparam, pad);
                         keyPress[pad] &=~(1<<i);
                         keyRelease[pad] |= (1<<i);
-                        break;
-                    }
-                }
-            }
-
-            event.evt = KEYRELEASE;
-            event.key = wParam;
+		        event.evt = KEYRELEASE;
+		        event.key = wParam;
 			break;
 
 		case WM_LBUTTONDOWN:

plugins/zeropad/zeropad.h
@@ -71,8 +71,11 @@ extern char libraryName[256];
 #define PADOPTION_REVERTRX 0x8
 #define PADOPTION_REVERTRY 0x10
 
+#define PADSUBKEYS 2
+extern int PadEnum[2][2];
+
 typedef struct {
-	unsigned long keys[2][PADKEYS];
+	unsigned long keys[2 * PADSUBKEYS][PADKEYS];
 	int log;
     int options;  // upper 16 bits are for pad2
 } PADconf;
@@ -128,6 +131,13 @@ void LoadConfig();
 void SaveConfig();
 
 void SysMessage(char *fmt, ...);
+
+inline int FindKey(int key, int pad) {
+	for (int p=0; p < PADSUBKEYS; p++)
+		for (int i=0; i<PADKEYS; i++)
+			if (key == conf.keys[(PadEnum[pad][p])][i])
+		   		return i;
+}
 
 #endif
 

plugins/zeropad/Linux/callbacks.h
@@ -10,19 +10,19 @@ OnConf_Pad2                            (GtkButton       *button,
                                         gpointer         user_data);
 
 void
-OnConf_Key                             (GtkButton       *button,
+OnConf_Pad3                            (GtkButton       *button,
                                         gpointer         user_data);
 
 void
-OnConf_Cancel                          (GtkButton       *button,
-                                        gpointer         user_data);
-
-void
-OnConf_Ok                              (GtkButton       *button,
+OnConf_Pad4                            (GtkButton       *button,
                                         gpointer         user_data);
 
 void
 on_joydevicescombo_changed             (GtkComboBox     *combobox,
+                                        gpointer         user_data);
+
+void
+OnConf_Key                             (GtkButton       *button,
                                         gpointer         user_data);
 
 void
@@ -43,4 +43,12 @@ on_checkbutton_reversery_toggled       (GtkToggleButton *togglebutton,
 
 void
 on_forcefeedback_toggled               (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+OnConf_Cancel                          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+OnConf_Ok                              (GtkButton       *button,
                                         gpointer         user_data);

commit 265319f4cb4f682ad06aaebc4efc208ec39cb57a

ZeroPad: Patch to fix a bug introduced in r1125.
git-svn-id: http://pcsx2.googlecode.com/svn/trunk@1128 96395faa-99c1-11dd-bbfe-3dabce05a288

plugins/zeropad/Linux/linux.cpp
@@ -317,6 +317,7 @@ int _GetJoystickIdFromPAD(int pad)
 			}
 		}
 	}
+	return joyid;
 }
 
 void CALLBACK PADupdate(int pad)
@@ -382,9 +383,10 @@ void CALLBACK PADupdate(int pad)
 #endif
 
 				i = FindKey(key, pad);
-				keyPress |= (1 << i);
-				keyRelease &= ~(1 << i);
-
+				if (i != -1) {
+					keyPress |= (1 << i);
+					keyRelease &= ~(1 << i);
+				}
 				event.evt = KEYPRESS;
 				event.key = key;
 				break;
@@ -423,8 +425,10 @@ void CALLBACK PADupdate(int pad)
 #endif
 
 				i = FindKey(key, pad);
-				keyPress &= ~(1 << i);
-				keyRelease |= (1 << i);
+				if (i != -1) {
+					keyPress &= ~(1 << i); 
+					keyRelease |= (1 << i);
+				}
 				event.evt = KEYRELEASE;
 				event.key = key;
 				break;

plugins/zeropad/Windows/win.cpp
@@ -190,17 +190,21 @@ LRESULT WINAPI PADwndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			if (lParam & 0x40000000)
 				return TRUE;
 
-			i = FindKey(wparam, pad);
-			keyPress[pad] |= (1 << i);
-			keyRelease[pad] &= ~(1 << i);
+			i = FindKey(wParam, pad);
+			if (i != -1) {
+				keyPress[pad] |= (1 << i);
+				keyRelease[pad] &= ~(1 << i);
+			}
 			event.evt = KEYPRESS;
 			event.key = wParam;
 			break;
 
 		case WM_KEYUP:
-			i = FindKey(wparam, pad);
-			keyPress[pad] &= ~(1 << i);
-			keyRelease[pad] |= (1 << i);
+			i = FindKey(wParam, pad);
+			if (i != -1) {
+				keyPress[pad] &= ~(1 << i);
+				keyRelease[pad] |= (1 << i);
+			}
 			event.evt = KEYRELEASE;
 			event.key = wParam;
 			break;

plugins/zeropad/zeropad.h
@@ -162,6 +162,7 @@ inline int FindKey(int key, int pad)
 	for (int p = 0; p < PADSUBKEYS; p++)
 		for (int i = 0; i < PADKEYS; i++)
 			if (key == conf.keys[(PadEnum[pad][p])][i]) return i;
+	return -1;
 }
 
 #endif