4266af81e76a85be06f92252fb989c88541835cf
gregory.hainaut <gregory.hainaut@96395faa-99c1-11dd-bbfe-3dabce05a288>
2014-01-26 12:00:38 +0000

    pcsx2: restore linux compilation (remove the bad line as a temporary solution) all: correct various printf format warning
    git-svn-id: http://pcsx2.googlecode.com/svn/trunk@5842 96395faa-99c1-11dd-bbfe-3dabce05a288

--- pcsx2/Patch.cpp
+++ pcsx2/Patch.cpp
@@ -225,8 +225,10 @@ int LoadCheatsFromZip(wxString gameCRC, const wxString& cheatsArchiveFilename) {
     wxString name = entry->GetName();
     name.MakeUpper();
     if (name.Find(gameCRC) == 0 && name.Find(L".PNACH") == name.Length()-6) {
+#ifndef __LINUX__
       Console.WriteLn(Color_Gray, "Loading patch '%s' from archive '%s'",
                       entry->GetName().To8BitData(), cheatsArchiveFilename.To8BitData());
+#endif
       wxTextInputStream pnach(zip);
       while (!zip.Eof()) {
         inifile_processString(pnach.ReadLine());
